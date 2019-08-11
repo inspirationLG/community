aadabbe51caf00badd5248da2bb6efad4783f506
inspirationLG <hchienisabad@gmail.com>
2019-08-12 00:23:10 +0800

    完成更新问题功能

--- src/main/java/cn/edu/zju/community/Controller/PublishController.java
+++ src/main/java/cn/edu/zju/community/Controller/PublishController.java
@@ -1,12 +1,14 @@
 package cn.edu.zju.community.Controller;
 
-import cn.edu.zju.community.Mapper.QuestionMapper;
+import cn.edu.zju.community.DTO.QuestionDTO;
 import cn.edu.zju.community.Model.Question;
 import cn.edu.zju.community.Model.User;
+import cn.edu.zju.community.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,48 +19,62 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class PublishController {
+
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+        return "publish";
+    }
+
     @GetMapping("/publish")
     public String publish() {
         return "publish";
     }
+
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam("title") String title,
-            @RequestParam("description") String description,
-            @RequestParam("tag") String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Integer id,
             HttpServletRequest request,
             Model model) {
-        model.addAttribute ("title", title);
-        model.addAttribute ("description", description);
-        model.addAttribute ("tag", tag);
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
         if (title == null || title == "") {
-            model.addAttribute ("error", "标题不能为空");
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
         if (description == null || description == "") {
-            model.addAttribute ("error", "问题补充不能为空");
+            model.addAttribute("error", "问题补充不能为空");
             return "publish";
         }
         if (tag == null || tag == "") {
-            model.addAttribute ("error", "标签不能为空");
+            model.addAttribute("error", "标签不能为空");
             return "publish";
         }
         User user = (User) request.getSession().getAttribute("user");
 
         if (user == null) {
-            model.addAttribute ("error", "用户未登录");
+            model.addAttribute("error", "用户未登录");
             return "publish";
         }
-        Question question = new Question ();
-        question.setTitle (title);
-        question.setDescription (description);
-        question.setTag (tag);
-        question.setCreator (user.getId ());
-        question.setGmtCreate (System.currentTimeMillis ());
-        question.setGmtModified (question.getGmtCreate ());
-        questionMapper.create (question);
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setId(id);
+        questionService.createOrUpdate(question);
+//        questionMapper.create(question);
         return "redirect:/";
     }
 }
--- src/main/java/cn/edu/zju/community/Mapper/QuestionMapper.java
+++ src/main/java/cn/edu/zju/community/Mapper/QuestionMapper.java
@@ -1,10 +1,7 @@
 package cn.edu.zju.community.Mapper;
 
 import cn.edu.zju.community.Model.Question;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -30,4 +27,7 @@ public interface QuestionMapper {
 
     @Select("select * from question where id = #{id}")
     Question getById(@Param("id") Integer id);
+
+    @Update("update question set title = #{title}, description = #{description}, gmt_modified = #{gmtModified}, tag = #{tag} where id = #{id}")
+    void update(Question question);
 }
