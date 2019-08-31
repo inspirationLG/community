7ac7badc1a6ad40933a40bd75454daf1580946f1
inspirationLG <hchienisabad@gmail.com>
2019-08-31 10:07:51 +0800

    12231

--- src/main/java/cn/edu/zju/community/mapper/QuestionMapper.java
+++ src/main/java/cn/edu/zju/community/mapper/QuestionMapper.java
@@ -28,6 +28,6 @@ public interface QuestionMapper {
     @Select("select * from question where id = #{id}")
     Question getById(@Param("id") Integer id);
 
-    @Update("update2222 question set title = #{title}, description = #{description}, gmt_modified = #{gmtModified}, tag = #{tag} where id = #{id}")
+    @Update("update question set title = #{title}, description = #{description}, gmt_modified = #{gmtModified}, tag = #{tag} where id = #{id}")
     void update(Question question);
 }
